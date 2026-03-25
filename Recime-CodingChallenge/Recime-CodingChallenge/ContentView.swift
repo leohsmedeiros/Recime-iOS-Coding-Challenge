3a8f79f8d92877ae553963cc0a9b9f6937de9672
Leonardo Medeiros <leohsmedeiros@gmail.com>
2026-03-24 22:19:08 -0300

    Initial commit

---
+++ Recime-CodingChallenge/Recime-CodingChallenge/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Recime-CodingChallenge
+//
+//  Created by Leonardo Medeiros on 24/03/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Recime-CodingChallenge/Recime-CodingChallenge/Recime_CodingChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recime_CodingChallengeApp.swift
+//  Recime-CodingChallenge
+//
+//  Created by Leonardo Medeiros on 24/03/26.
+//
+
+import SwiftUI
+
+@main
+struct Recime_CodingChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
